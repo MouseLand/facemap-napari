d38ed4fce2f95dcaabf544deaecff93ea47cafa0
Atika.Syeda <syeda.atika@ucalgary.ca>
2021-05-31 10:24:46 +0300

    initial commit

---
+++ facemap_napari/_tests/test_function.py
@@ -0,0 +1,7 @@
+# from facemap_napari import threshold, image_arithmetic
+
+# add your tests here...
+
+
+def test_something():
+    pass
---
+++ facemap_napari/_tests/test_writer.py
@@ -0,0 +1,7 @@
+# from facemap_napari import napari_get_writer, napari_write_image
+
+# add your tests here...
+
+
+def test_something():
+    pass
